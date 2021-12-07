d370c0e254be4adf911773d670414ce0d018b170
Reece Mickelson <spartannumber2@gmail.com>
2021-12-06 19:57:40 -0700

    Update preston_alert.js

--- weather/js/preston_alert.js
+++ weather/js/preston_alert.js
@@ -2,7 +2,7 @@ function showAlert() {
     const date = new Date();
     let day = date.getDate();
 
-    if (day == 5 || 6){
+    if (day == 5){
         document.getElementById('heads-up').style.display = 'block';
         document.getElementById('heads-up').style.fontWeight = 'bold';
     }
